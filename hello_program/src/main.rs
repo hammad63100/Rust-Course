54217a219973b1909665e4efa4baeb39dfe51a1b
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2025-03-17 15:25:36 +0500

    debug target

--- hello_program/src/main.rs
+++ hello_program/src/main.rs
@@ -4,12 +4,19 @@
 // }
 
 
+// fn main() {
+    
+//     let num:u32= 255;
+//     print!("The number is: {}\n", num);   
+
+//     let num:u32 = 200;
+
+//     print!("The number is: {}", num);
+// }
+
+
+
+// str string data type
 fn main() {
     
-    let  num= 255;
-    print!("The number is: {}", num);   
-
-    let num = 200;
-
-    print!("The number is: {}", num);
 }
