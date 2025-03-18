6d1e9763d15dfdf31da2cfb032850f3cc8d75376
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2025-03-18 14:53:34 +0500

    Refactor main function to use String type and update greeting message

--- hello_program/src/main.rs
+++ hello_program/src/main.rs
@@ -18,6 +18,13 @@
 
 // &str &  string data type
 fn main() {
-    let string_latraliy = "Hello, Hammad";
-    println!("{}", string_latraliy);
+    // let string_latraliy = "Hello, Hammad";
+    // println!("{}", string_latraliy);
+
+
+    let mut string_object:String = String::from("Hello, Hammad");
+
+    string_object.push_str(" How are you?");
+    println!("{}", string_object);
+
 }
