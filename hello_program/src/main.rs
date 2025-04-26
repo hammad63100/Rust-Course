94fafd31e44040286f5e02fe48cd7c6705244f21
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2025-04-26 15:30:29 +0500

    Implements basic arithmetic functions
    Adds `add` function to calculate the sum of two numbers
    Updates `main` function to demonstrate usage of the `add` function
    Removes commented-out code for clarity
    
    No related issues

--- hello_program/src/main.rs
+++ hello_program/src/main.rs
@@ -40,17 +40,44 @@
 // }
 
 //tupple
-fn main(){
-    let emp_info:(&str, u8) = ("Hammad", 25);
+// fn main(){
+//     let emp_info:(&str, u8) = ("Hammad", 25);
 
-    let emp_name = emp_info.0;
-    let emp_age = emp_info.1;
+//     let emp_name = emp_info.0;
+//     let emp_age = emp_info.1;
 
-    println!("{} is {} years old", emp_name, emp_age);
+//     println!("{} is {} years old", emp_name, emp_age);
 
-    // destructuring
+//     // destructuring
 
-    let (employee_name, employee_age) = emp_info;
-    println!("{} is {} years old", employee_name, employee_age);
+//     let (employee_name, employee_age) = emp_info;
+//     println!("{} is {} years old", employee_name, employee_age);
 
+// }
+
+
+// function overview
+
+// fn main(){
+    
+//     print_value(5);
+// }
+
+// // fn print_value(){
+// //     println!("{}", 6+6);
+// // }
+// fn print_value(item : u8){
+//     println!("{}",item);
+// }
+
+
+fn main() {
+    let num1:u8 = 5;
+    let num2:u8 = 10;
+    let result:u8 = add(num1, num2);
+    println!("The sum of {} and {} is {}", num1, num2, result);
+}
+
+fn add(num1: u8, num2: u8) -> u8 {
+    num1 + num2
 }
